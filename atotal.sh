a6eca6df8234a35a8bf579951ab1733c8aeda036
Borja2001 <bopres@floridauniversitaria.es>
2023-02-14 15:35:06 +0100

    Finalizando atotal.sh

---
+++ atotal.sh
@@ -0,0 +1,18 @@
+numLin=$((`cat consumos.txt | wc -l`+1))
+consumo=0
+while [ $consumo -eq 0 ]
+do 
+read -p "Dame la ciudad: " ciud
+    cont=1
+    while [ $cont -le $numLin ]
+    do
+    ciudad=`cat consumos.txt | head -n$cont | tail -n1 |awk '{print $1}'`
+    if [ $ciudad = $ciud ]
+    then 
+    parteConsumo=`cat consumos.txt | head -n$cont | tail -n1 | awk '{print $4}'`
+    consumo=$(($consumo+$parteConsumo))
+    fi
+    cont=$(($cont+1))
+    done
+echo $consumo
+done
